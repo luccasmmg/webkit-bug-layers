aaa83f4b0e7cb07b471bf362dec7e8c74d8f3d29
Luccas Mateus <luccasmmg@gmail.com>
2024-02-22 16:05:11 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { DecodedLayer } from "@vizzuality/layer-manager-layers-deckgl";
 import { Layer, LayerManager } from "@vizzuality/layer-manager-react";
 import PluginMapboxGl from "@vizzuality/layer-manager-plugin-mapboxgl";
 import CartoProvider from "@vizzuality/layer-manager-provider-carto";
-import './App.css'
+import "./App.css";
 
 const cartoProvider = new CartoProvider();
 
@@ -19,17 +19,53 @@ const alert = {
   deck: [
     new MapboxLayer({
       decodeFunction: `
-  float r = color.r * 255.;
-  alpha = r;
+    float agreementValue = alpha * 255.;
+    float r = color.r * 255.;
+    float g = color.g * 255.;
+    float b = color.b * 255.;
+    float day = r * 255. + g;
+    float confidence = floor(b / 100.) - 1.;
+    float intensity = mod(b, 100.) * 150.;
+    if (
+      day > 0. &&
+      day >= startDayIndex &&
+      day <= endDayIndex &&
+      agreementValue > 0.
+    )
+    {
+      if (intensity > 255.) {
+        intensity = 255.;
+      }
+      float confidenceValue = 0.;
+      if (confirmedOnly > 0.) {
+        confidenceValue = 255.;
+      }
+      if (agreementValue == 4. || agreementValue == 16. || agreementValue == 64.) {
+        color.r = 237. / 255.;
+        color.g = 164. / 255.;
+        color.b = 194. / 255.;
+        alpha = (intensity -confidenceValue) / 255.;
+      } else if (agreementValue == 8. || agreementValue == 32. || agreementValue ==  128.){
+        color.r = 220. / 255.;
+        color.g = 102. / 255.;
+        color.b = 153. / 255.;
+        alpha = intensity / 255.;
+      } else {
+        color.r = 201. / 255.;
+        color.g = 42. / 255.;
+        color.b = 109. / 255.;
+        alpha = intensity / 255.;
+      }
+    } else {
+      alpha = 0.;
+    }
 `,
       decodeParams: {
         startDayIndex: 2785,
         endDayIndex: 3334,
-        numberOfDays: 3334,
         confirmedOnly: 0,
       },
       id: "integrated",
-      premultipliedAlpha: true,
       type: TileLayer,
       data: "https://tiles.globalforestwatch.org/umd_glad_landsat_alerts/latest/default/{z}/{x}/{y}.png",
       tileSize: 256,
@@ -56,7 +92,6 @@ const alert = {
           return new DecodedLayer({
             id: subLayerId,
             image: data,
-            premultipliedAlpha: true,
             bounds: [west, south, east, north],
             textureParameters: {
               [GL.TEXTURE_MIN_FILTER]: GL.NEAREST,
